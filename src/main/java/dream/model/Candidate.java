3738730f7ca0c085cdc846e06201e5a4e4200f1f
xocer <vitamin9292@mail.ru>
2021-02-16 16:19:25 +0300

    candidates.jsp - список кандидатов

--- src/main/java/dream/model/Candidate.java
+++ src/main/java/dream/model/Candidate.java
@@ -1,4 +1,14 @@
 package dream.model;
 
+import lombok.Data;
+
+@Data
 public class Candidate {
+    private int id;
+    private String name;
+
+    public Candidate(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
--- src/main/java/dream/store/Store.java
+++ src/main/java/dream/store/Store.java
@@ -1,5 +1,6 @@
 package dream.store;
 
+import dream.model.Candidate;
 import dream.model.Post;
 
 import java.time.LocalDate;
@@ -13,17 +14,26 @@ public class Store {
 
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job", "For new worker", LocalDate.now()));
         posts.put(2, new Post(2, "Middle Java Job", "For normal worker", LocalDate.now()));
         posts.put(3, new Post(3, "Senior Java Job", "For very professional worker", LocalDate.now()));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
     public static Store instOf() {
         return INST;
     }
 
-    public Collection<Post> findAll() {
+    public Collection<Post> findAllPosts() {
         return posts.values();
+    }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
     }
 }

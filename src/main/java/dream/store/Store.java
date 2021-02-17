71f0b3ae780cbc1c8a5415015971013a46b52841
xocer <vitamin9292@mail.ru>
2021-02-17 20:05:09 +0300

    CandidateServlet. Создание кандидата.

--- src/main/java/dream/store/Store.java
+++ src/main/java/dream/store/Store.java
@@ -12,6 +12,7 @@ public class Store {
 
     private static final Store INST = new Store();
     private static AtomicInteger POST_ID = new AtomicInteger(4);
+    private static AtomicInteger CANDIDATE_ID = new AtomicInteger(4);
 
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
@@ -33,6 +34,11 @@ public class Store {
     public void save(Post post) {
         post.setId(POST_ID.incrementAndGet());
         posts.put(post.getId(), post);
+    }
+
+    public void save(Candidate candidate) {
+        candidate.setId(CANDIDATE_ID.incrementAndGet());
+        candidates.put(candidate.getId(), candidate);
     }
 
     public Collection<Post> findAllPosts() {

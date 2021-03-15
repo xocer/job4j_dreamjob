a30d4e66bbbd7e4134bb52052075ff79a1ebf1cf
xocer <vitamin9292@mail.ru>
2021-03-16 00:51:39 +0300

    JSON формат

--- src/main/java/dream/servlet/GreetingServlet.java
+++ src/main/java/dream/servlet/GreetingServlet.java
@@ -1,5 +1,7 @@
 package dream.servlet;
 
+import com.alibaba.fastjson.JSON;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,11 +13,11 @@ public class GreetingServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/plain");
+        resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
         String name = req.getParameter("name");
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.println("Nice to meet you, " + name);
+        writer.println(JSON.toJSONString("Nice to meet you, " + name));
         writer.flush();
     }
 }
